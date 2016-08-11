d3f7f6799e3f4ee7ea45a2a18f27cdf72ea66517
lmizrahi12 <lmizrahi12@gmail.com>
2016-08-11 21:32:03 +0200

    Updated Lab 2 to have a nice little menu

--- Lab 3/trapezoid_integration.cpp
+++ Lab 3/trapezoid_integration.cpp
@@ -9,21 +9,48 @@
 using namespace std;
 
 double f(int x);
+void Test_noOfThreads();
+void Test_noOfPartitions();
 
 int main () {
+    int n = 0;
+
+    while(n != 1 && n != 2){
+        cout << "Which test?" << endl;
+        cout << "\t 1.Number of Partitions" << endl;
+        cout << "\t 2.Number of Threads" << endl;
+
+        cin >> n;
+
+        if(n != 1 && n != 2)
+            cout << "Invalid choice  t('^'t)" << endl;
+    }
+
+    if(n == 1)
+        Test_noOfPartitions();
+
+    else
+        Test_noOfThreads();
+}
+
+double f(int x){
+    return x*exp(-x);
+}
+
+void Test_noOfThreads(){
     ofstream file;
     int N = 500;
     long double area;
     double width;
-    int n;
+    int noThreads;
 
     width = (double)20/(N);    
 
-    for(int n = 1;n <= 1000;n++){
+    for(noThreads = 1;noThreads <= 1000;noThreads++){
         area = 0;
 
         double start = omp_get_wtime(); 
-            omp_set_num_threads(n);
+            omp_set_num_threads(noThreads);
             #pragma omp parallel for reduction(+:area)
             for(int i = 0;i < N;i++){
                 area += ( f(i*width) + f((i+1) * width) )/2;
@@ -32,18 +59,47 @@ int main () {
             area *= width;
         double end = omp_get_wtime();
 
-        cout << "Threads " << n << ", " << "Area: " << area << ", " << "Time: " << end-start << endl;
+        cout << "Threads " << noThreads << ", " << "Area: " << area << ", " << "Time: " << end-start << endl;
 
-        file.open("results_trapezoid_threads_accuracy.csv", ios::app);
-        file << n << "," << area << endl;
+        /*file.open("results_trapezoid_threads_accuracy.csv", ios::app);
+        file << noThreads << "," << area << endl;
         file.close();
 
         file.open("results_trapezoid_threads_time.csv", ios::app);
-        file << n << "," << end-start << endl;
-        file.close();
+        file << noThreads << "," << end-start << endl;
+        file.close();*/
     }
 }
 
-double f(int x){
-    return x*exp(-x);
+void Test_noOfPartitions(){
+    ofstream file;
+    int N;
+    long double area;
+    double width;
+    int noThreads = 4;    
+
+    for(N = 1;N <= 1000;N++){
+        area = 0;
+        width = (double)20/(N);
+
+        double start = omp_get_wtime(); 
+            omp_set_num_threads(noThreads);
+            #pragma omp parallel for reduction(+:area)
+            for(int i = 0;i < N;i++){
+                area += ( f(i*width) + f((i+1) * width) )/2;
+            }
+
+            area *= width;
+        double end = omp_get_wtime();
+
+        cout << "Partitions " << N << ", " << "Area: " << area << ", " << "Time: " << end-start << endl;
+
+        /*file.open("temp_results_trapezoid_partitions_accuracy.csv", ios::app);
+        file << N << "," << area << endl;
+        file.close();
+
+        file.open("results_trapezoid_partitions_time.csv", ios::app);
+        file << N << "," << end-start << endl;
+        file.close();*/
+    }
 }
